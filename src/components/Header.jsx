3ff4e72c19ee140b1f63e2f53e4ee0e4262f6055
ottert <thanhduyly18@gmail.com>
2024-07-21 10:24:40 +0700

    firts commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function Header({ onSearch }) {
+  const [textSearch, setSearch] = useState("");
+  return (
+    <div className="p-4 bg-black flex items-center justify-between">
+      <div className="flex items-center space-x-4">
+        <h1 className="text-[40px] uppercase font-bold text-red-700">Movie</h1>
+        <nav className="flex items-center space-x-4">
+          <a href="#" className="text-white">
+            Home
+          </a>
+          <a href="#" className="text-white">
+            About
+          </a>
+          <a href="#" className="text-white">
+            Contact
+          </a>
+        </nav>
+      </div>
+      <div className="flex items-center space-x-4">
+        <input
+          type="text"
+          placeholder="Search"
+          className="p-4 text-black"
+          onChange={(e) => setSearch(e.target.value)}
+          value={textSearch}
+        />
+        <button
+          className="p-2 text-white bg-red-600"
+          onClick={() => onSearch(textSearch)}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+}
+export default Header;
